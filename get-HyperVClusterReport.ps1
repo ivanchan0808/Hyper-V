6ad699f25986830edbb80fe5c09281e0e782ef93
Ivan <42667714+ivanchan0808@users.noreply.github.com>
2023-06-01 01:21:04 +0800

    Initial file
    Gather Hyper-V VM lists

---
+++ get-HyperVClusterReport.ps1
@@ -0,0 +1,22 @@
+﻿$cluster=get-cluster
+$filepath="C:\$cluster-ClusterVMReport.csv"
+
+Write-host "Collecting below cluster node information........."
+Get-ClusterNode | Select-Object @{label = 'VM Name'; expression = { $_.Name } }
+Write-Host "Generating "$cluster.name" VM list....... Check path $filepath"
+
+Get-VM –ComputerName (Get-ClusterNode) | `
+% { $VMinst = Get-VHD -ComputerName $_.ComputerName -VmId $_.VmId; $VMinst | `
+Add-Member -NotePropertyName "Name" -NotePropertyValue $_.Name; $VMinst | `
+Add-Member -NotePropertyName "ProcessorCount" -NotePropertyValue $_.ProcessorCount; $VMinst | `
+Add-Member -NotePropertyName "MemoryStartup" -NotePropertyValue $_.MemoryStartup; $VMinst | `
+Add-Member -NotePropertyName "NetworkAdapters" -NotePropertyValue $_.NetworkAdapters.IPAddresses; $VMinst | `
+Add-Member -NotePropertyName "State" -NotePropertyValue $_.state; $VMinst } | `
+Select-Object @{label = 'VM Name'; expression = { $_.Name } }, @{label = 'vCPU'; expression = { $_.ProcessorCount } }, `
+@{label = 'vMem (GB)'; expression = { $_.MemoryStartup / 1gb –as [int] } }, `
+@{label = 'IP Addresses'; expression = { $_.NetworkAdapters } }, @{label = 'Host Name'; expression = { $_.ComputerName } }, `
+Path, VhdFormat, VhdType, @{label = 'Datastore (GB)'; expression = { $_.FileSize / 1gb –as [int] } }, `
+@{label = 'Max Disk Size (GB)'; expression = { $_.Size / 1gb –as [int] } }, `
+@{label = 'Remaining Space (GB)'; expression = { ($_.Size / 1gb - $_.FileSize / 1gb) –as [int] } }, `
+@{label = 'State'; expression = { ($_.State) } } | `
+Export-Csv -Path $filepath -NoTypeInformation
---
+++ get-HyperVReport.ps1
@@ -0,0 +1,19 @@
+﻿$computername=hostname
+$filepath="C:\$computername-VHDReport.csv"
+Write-Host Generating $computername VM list....... Check path $filepath
+
+Get-VM | `
+% { $vhd = Get-VHD -ComputerName $_.ComputerName -VmId $_.VmId; $vhd | `
+Add-Member -NotePropertyName "Name" -NotePropertyValue $_.Name; $vhd | `
+Add-Member -NotePropertyName "ProcessorCount" -NotePropertyValue $_.ProcessorCount; $vhd | `
+Add-Member -NotePropertyName "MemoryStartup" -NotePropertyValue $_.MemoryStartup; $vhd | `
+Add-Member -NotePropertyName "NetworkAdapters" -NotePropertyValue $_.NetworkAdapters.IPAddresses; $vhd | `
+Add-Member -NotePropertyName "State" -NotePropertyValue $_.state; $vhd } | `
+Select-Object @{label = 'VM Name'; expression = { $_.Name } }, @{label = 'vCPU'; expression = { $_.ProcessorCount } }, `
+@{label = 'vMem (GB)'; expression = { $_.MemoryStartup / 1gb –as [int] } }, `
+@{label = 'IP Addresses'; expression = { $_.NetworkAdapters } }, @{label = 'Host Name'; expression = { $_.ComputerName } }, `
+Path, VhdFormat, VhdType, @{label = 'Size On Physical Disk (GB)'; expression = { $_.FileSize / 1gb –as [int] } }, `
+@{label = 'Max Disk Size (GB)'; expression = { $_.Size / 1gb –as [int] } }, `
+@{label = 'Remaining Space (GB)'; expression = { ($_.Size / 1gb - $_.FileSize / 1gb) –as [int] } }, `
+@{label = 'State'; expression = { ($_.State) } } | `
+Export-Csv -Path $filepath -NoTypeInformation
